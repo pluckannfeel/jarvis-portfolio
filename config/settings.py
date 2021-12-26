689d5c3a63c5cd132d4c0942fdb41a9fbb2a8ba8
pluckannfeel <padayao.jm96@gmail.com>
2021-12-26 21:16:46 +0900

    updated settings.py

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
+DATABASES = {'default': env.dj_db_url("DATABASE_URL")}
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
